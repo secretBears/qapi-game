8a36f7eece8a601ab945764a30c83c9d5c4ccdf5
Cantado <slaimer.mmt-b2012@fh-salzburg.ac.at>
2014-06-16 12:00:01 +0000

    remove unused code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,35 +4,30 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show]
 
   def index
-    #render :json => :user => current_user, :status => 200
     @users = User.all
     render 'index'
   end
 
   def show
-    render :json => {:info => "Show User", :user => @user}, :status => 200
   end
 
   def create
     @user = User.create(user_params)
     if @user.valid?
       sign_in(@user)
-      render :json => {:info => "user created", :user => @user}, :status => 200
-      #respond_with @user, :location => users_path
+      render 'show'
     else
       render :json => {:info => "user created error", :error => @user.error}, :status => 500
-      #respond_with @user.errors, :location => users_path
     end
   end
 
   def update
-    #respond_with 
     User.update(current_user.id, user_params)
-    render :json => {:info => "user updated", :user => current_user}, :status => 200
+    @user = current_user
+    render 'show'
   end
 
   def destroy
-    #respond_with 
     User.find(current_user.id).destroy
     render :json => {:info => "user destroyed"}, :status => 200
   end
